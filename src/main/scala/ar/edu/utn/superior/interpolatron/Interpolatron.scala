1c81f41a000785e4c86d05f56ddd0bd275f2562e
Rodrigo López Dato <rlopezdato@frba.utn.edu.ar>
2013-09-06 18:25:55 -0300

    Agregadas validaciones y mejorado encapsulamiento

--- src/main/scala/ar/edu/utn/superior/interpolatron/Interpolatron.scala
+++ src/main/scala/ar/edu/utn/superior/interpolatron/Interpolatron.scala
@@ -1,8 +1,10 @@
+package ar.edu.utn.superior.interpolatron
+
 class Interpolatron {
 	type Punto = Tuple2[Double, Double]
-	var puntos = Set[Punto]()
+	private var puntos = Set[Punto]()
 
-	def difDiv(xi: List[Punto]): Double = {
+	private def difDiv(xi: List[Punto]): Double = {
 		xi match {
 			case Nil => 0 // Evitar warning
 			case x :: Nil => x._2
@@ -10,7 +12,24 @@ class Interpolatron {
 		}
 	}
 
-	def diferenciaDividida(n: Integer) = difDiv(puntos.toList take n)
+	private def diferenciaDividida(n: Integer) = difDiv(puntos.toList take n)
+	
+	private def abscisas = puntos map {p => p._1}
+	
+	private def verificarRepetido(punto: Punto) {
+	  if (abscisas contains punto._1) {
+	    throw new PuntoDuplicadoException
+	  }
+	} 
 
-	def diferenciasDivididas = (1 to puntos.size) map {i => diferenciaDividida(i)}
+	def diferenciasDivididas = {
+	  if (puntos isEmpty) throw new NingunPuntoIngresadoException
+	  (1 to puntos.size) map {i => diferenciaDividida(i)}
+	}
+	
+	def agregar(punto: Punto) = { verificarRepetido(punto); puntos += punto; this }
+	def quitar(punto: Punto) = { puntos -= punto; this }
 }
+
+class NingunPuntoIngresadoException extends RuntimeException
+class PuntoDuplicadoException extends RuntimeException
